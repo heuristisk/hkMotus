20902e2cc7ca7678ecb962d54dd74237abb17846
Anderson Santos Gusmão <anderson.santos.gusmao@gmail.com>
2017-09-12 22:21:51 -0300

    first commit

---
+++ hkMotus/hkMotus.h
@@ -0,0 +1,19 @@
+//
+//  hkMotus.h
+//  hkMotus
+//
+//  Created by Anderson Santos Gusmao on 12/09/17.
+//  Copyright © 2017 Heuristisk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for hkMotus.
+FOUNDATION_EXPORT double hkMotusVersionNumber;
+
+//! Project version string for hkMotus.
+FOUNDATION_EXPORT const unsigned char hkMotusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hkMotus/PublicHeader.h>
+
+
